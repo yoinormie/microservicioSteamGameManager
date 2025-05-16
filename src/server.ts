cb828396236b7f6ffd6a72f36cbc37adbc074509
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-16 10:05:12 +0200

    servidor usando las rutas + definido endpoint del microservicio

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@
  * Clase que inicia el servidor en el puerto 3000.
  * Importa app para que use ese servidor e importa la clase steamMicroservices para que permita usar sus endpoints.
  */
-import { env } from "process"
 import app from "./Config/config"
+import route from "./Routes/steamRoutes"
 import './steamMicroservices'
 
 const port = process.env.PORT
+const commonURLEndPoint = '/steam'
 
 app.listen(port, () => {
     console.log(`Microservicio inciado en el puerto ${port}`);
 })
+
+app.use(commonURLEndPoint, route)
