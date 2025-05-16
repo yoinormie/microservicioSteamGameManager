3e7f9a7143e23eb66695076f9dac4a0dea6aedd7
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-16 09:52:52 +0200

    Controllers hechos

--- src/Controller/steam.controller.ts
+++ src/Controller/steam.controller.ts
@@ -1,11 +1,38 @@
 /**
  * Fichero donde se definen los endpoints de este microservicio.
  */
-import express = require('express');
-import app from '../Config/config';
+import express from 'express';
+import { schemaGetUserStats, schemaGetUserSteamID, schemaMakeAchievementList } from '../Schemas/steam.schemas'
+import * as service from '../Services/steam.services'
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.json({
-        message: "Hola desde el microservicio de Steam"
-    });
-});
+export const getUserWithSteamID = async (req: Request, res: Response) => {
+    let bodyParsed = schemaGetUserSteamID.safeParse(req.body)
+    if (bodyParsed.success) {
+        var data = bodyParsed.data
+        if (data.isCustom) {
+            data.steamID = await service.getIDFromCustomURL(data.steamID)
+        }
+        var user = await service.getUserStats(data.steamID)
+        //Llamada a spring a guardar al usuario
+    }
+}
+
+export const getUserGames = async (req: Request, res: Response) => {
+    let bodyParsed = schemaGetUserStats.safeParse(req.body)
+    if (bodyParsed.success) {
+        var data = bodyParsed.data
+        const userGames = service.getOwnedGames(data.steamID)
+        //Llamada a guardar los juegos
+    }
+}
+
+export const getAchievementsInGames = async (req: Request, res: Response) => {
+    let bodyParsed = schemaMakeAchievementList.safeParse(req.body)
+    if (bodyParsed.success) {
+        const gameList: steamGame[] = bodyParsed.data.ownedGames.map(
+            (game: any) => new steamGame(game.appid, game.name, game.playtimeForever)
+        ); 
+        var gameProperties = service.makeAchievementsList(gameList, bodyParsed.data.steamID)
+        //Llamada a spring para guardar los logros
+    }
+}
