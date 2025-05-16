5ac964473f0fb9333e8c2d5dd52ac40f1ad7b7e8
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-16 10:03:31 +0200

    Rutas definidas

--- src/Routes/steamRoutes.ts
+++ src/Routes/steamRoutes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { ValidateBody } from "../Middleware/steam.middleware";
+import { schemaGetUserStats, schemaGetUserSteamID, schemaMakeAchievementList } from "../Schemas/steam.schemas";
+import { getAchievementsInGames, getUserGames, getUserWithSteamID } from "../Controller/steam.controller";
 
 const route = Router()
 
-route.
+route.post('/getsteamiduser', ValidateBody(schemaGetUserSteamID), getUserWithSteamID)
+route.post('/getuserslibrary', ValidateBody(schemaGetUserStats), getUserGames)
+route.post('/makeachievementslist', ValidateBody(schemaMakeAchievementList), getAchievementsInGames)
 
-export default router
+export default route
