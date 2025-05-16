4280b243b2010e3fcdae7e812ccfc8836fadb065
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-16 09:52:16 +0200

    Creados schemas faltantes

--- src/Schemas/steam.schemas.ts
+++ src/Schemas/steam.schemas.ts
@@ -9,3 +9,15 @@ export const schemaGetUserStats = z.object({
     steamID : z.string()
     .length(17,"El id no es válido")
 })
+
+export const steamGameSchema = z.object({
+  appid: z.number().int(),
+  name: z.string(),
+  playtimeForever: z.number().int().nonnegative(),
+});
+
+export const schemaMakeAchievementList = z.object({
+    steamID : z.string()
+    .length(17,"El id no es válido"),
+    ownedGames : z.array(steamGameSchema)
+})
